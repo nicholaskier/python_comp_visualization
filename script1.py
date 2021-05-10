9b86e3fded03754687549e8bec784f4f9a11a3c8
unknown <nicholaskier@gmail.com>
2021-05-10 17:30:14 -0500

    save resized image

--- script1.py
+++ script1.py
@@ -10,5 +10,6 @@ print(img.shape)
 
 resize_image=cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Galaxy", resize_image)
+cv2.imwrite("Galaxy_resized.jpg", resize_image)
 cv2.waitKey(2000)
 # closes window after 2 seconds
